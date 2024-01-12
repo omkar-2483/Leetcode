2e69b611ba2a115f1915b7d8aea616f34bd63ced
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2024-01-12 08:00:08 +0530

    Update LinkedList1.java

--- LinkedList1.java
+++ LinkedList1.java
@@ -33,45 +33,24 @@ Output: [8,9,9,9,0,0,0,1]*/
 class LinkedList1 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode ans=new ListNode();
-        ListNode head1=l1;
-        ListNode head2=l2;
-        ListNode head3=ans;
+        ListNode head=ans;
         int carry=0;
-        while(head1!=null && head2!=null){
-            head3.val=(head1.val+head2.val+carry)%10;
-            carry=(head1.val+head2.val+carry)/10;
-            if(head1.next!=null || head2.next!=null || carry!=0){
-                ListNode newNode=new ListNode();
-                head3.next=newNode;
+        while(l1!=null || l2!=null || carry!=0){
+            int sum=0;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
             }
-            head1=head1.next;
-            head2=head2.next;
-            head3=head3.next;
-        }
-        while(head1!=null){
-            head3.val=(head1.val+carry)%10;
-            carry=(head1.val+carry)/10;
-            if(head1.next!=null || carry!=0){
-                ListNode newNode=new ListNode();
-                head3.next=newNode;
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
             }
-            head1=head1.next;
-            head3=head3.next;
+            sum+=carry;
+            carry=sum/10;
+            ListNode newNode=new ListNode(sum%10);
+            head.next=newNode;
+            head=newNode;
         }
-        while(head2!=null){
-            head3.val=(head2.val+carry)%10;
-            carry=(head2.val+carry)/10;
-            if(head2.next!=null || carry!=0){
-                ListNode newNode=new ListNode();
-                head3.next=newNode;
-            }
-            head1=head2.next;
-            head3=head3.next;
-        }
-        if(carry!=0){
-            head3.next=null;
-            head3.val=carry;
-        }
-        return ans;
+        return ans.next;
     }
 }
