bbdd6e2572af0fcd9a64c4052369c10765e7c49d
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2024-08-21 10:54:10 +0530

    Time: 4 ms (86.4%), Space: 68.7 MB (25.19%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,27 +9,69 @@
  * }
  */
 class Solution {
+    // public boolean isPalindrome(ListNode head) {
+    //     int length=0;
+    //     ListNode temp=head;
+    //     while(temp!=null){
+    //         length++;
+    //         temp=temp.next;
+    //     }
+    //     Stack<Integer> st=new Stack<>();
+    //     temp=head;
+    //     for(int i=0;i<length/2;i++){
+    //         st.push(temp.val);
+    //         temp=temp.next;
+    //     }
+    //     if(length%2!=0){
+    //         temp=temp.next;
+    //     }
+    //     while(temp!=null){
+    //         if(st.pop()!=temp.val)
+    //             return false;
+    //         temp=temp.next;
+    //     }
+    //     return true;
+    // }
+
+
+    //constant space complexity
+
     public boolean isPalindrome(ListNode head) {
-        int length=0;
-        ListNode temp=head;
-        while(temp!=null){
-            length++;
-            temp=temp.next;
+        //base case
+        if(head == null || head.next == null) return true;
+        //find mid node
+        ListNode Mid = findMidNode(head);
+
+        //reverse second half
+        ListNode prev = null;
+        ListNode curr= Mid;
+        ListNode next;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
-        Stack<Integer> st=new Stack<>();
-        temp=head;
-        for(int i=0;i<length/2;i++){
-            st.push(temp.val);
-            temp=temp.next;
-        }
-        if(length%2!=0){
-            temp=temp.next;
-        }
-        while(temp!=null){
-            if(st.pop()!=temp.val)
-                return false;
-            temp=temp.next;
+
+        //check both halfs
+        ListNode right = prev;
+        ListNode left = head;
+        while(right != null){
+            if(right.val != left.val) return false;
+            left = left.next;
+            right = right.next;
         }
         return true;
+    }
+
+
+    public ListNode findMidNode(ListNode head){
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast !=null && fast.next!=null){
+            slow = slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
     }
 }
