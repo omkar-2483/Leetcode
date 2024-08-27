005c6c430ca3cd28993183fb1bf48039ffff9539
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2024-08-27 22:31:07 +0530

    Time: 0 ms (100%), Space: 41 MB (78.42%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
@@ -1,15 +1,29 @@
 class Solution {
+
+    //brute force
+    // public int timeRequiredToBuy(int[] tickets, int k) {
+    //     int time=0;
+    //     while(tickets[k]!=0){
+    //         for(int i=0; i<tickets.length; i++){
+    //             if(tickets[k]==0) break;
+    //             if(tickets[i]!=0){
+    //                 tickets[i]--;
+    //                 time++;
+    //             }
+    //         }
+    //     }
+    //     return time;
+    // }
+
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int time=0;
-        while(tickets[k]!=0){
-            for(int i=0; i<tickets.length; i++){
-                if(tickets[k]==0) break;
-                if(tickets[i]!=0){
-                    tickets[i]--;
-                    time++;
-                }
-            }
+        int sec = 0;
+        int pos = tickets.length - (k+1);
+        for(int i = 0; i < tickets.length; i++){
+            if(i <= k)
+                sec += Math.min(tickets[k], tickets[i]);
+            else
+                sec += Math.min(tickets[k]-1, tickets[i]);
         }
-        return time;
+        return sec;
     }
 }
