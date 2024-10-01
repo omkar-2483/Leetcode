3b0031243a4925f56dff767882e37a6f154d6866
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2024-10-01 11:44:50 +0530

    Time: 0 ms (100%), Space: 42 MB (64.53%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,41 +14,16 @@
  * }
  */
 class Solution {
-    private class Info{
-        TreeNode node;
-        int depth;
-
-        public Info(TreeNode node, int depth){
-            this.node = node;
-            this.depth=depth;
-        }
-    }
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans= new LinkedList<>();
-        if(root==null) return ans;
-        
-        Queue<Info> q= new LinkedList<>();
-        HashMap<Integer, TreeNode> map= new HashMap<>();
-        int maxDepth=0;
+        List<Integer> list = new LinkedList<>();
+        rightView(root, list, 0);
+        return list;
+    }
 
-        q.add(new Info(root, 0));
-        while(!q.isEmpty()){
-            Info curr=q.remove();
-            if(!map.containsKey(curr.depth)){
-                map.put(curr.depth, curr.node);
-                maxDepth=curr.depth;
-            }
-            if(curr.node.right!=null){
-                q.add(new Info(curr.node.right, curr.depth+1));
-            }
-             if(curr.node.left!=null){
-                q.add(new Info(curr.node.left, curr.depth+1));
-            }
-        }
-
-        for(int i=0; i<=maxDepth; i++){
-            ans.add(map.get(i).val);
-        }
-        return ans;
+    private void rightView(TreeNode root, List<Integer> list, int level){
+        if(root==null) return;
+        if(level==list.size()) list.add(root.val);
+        rightView(root.right, list, level+1);
+        rightView(root.left, list, level+1);
     }
 }
