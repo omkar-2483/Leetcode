a151e653efc0ead76d9315e5ccdf34f3928da7f1
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2024-10-03 16:31:05 +0530

    Time: 0 ms (100%), Space: 44.2 MB (27.19%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,20 +13,39 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         List<Integer> list=new ArrayList<>();
+//         inorder(root, list);
+//         for(int i=0;i<list.size()-1;i++){
+//             if(list.get(i)>=list.get(i+1)) return false;
+//         }
+//         return true;
+
+//     }
+//     private void inorder(TreeNode root, List<Integer> list){
+//         if(root==null) return;
+//         inorder(root.left, list);
+//         list.add(root.val);
+//         inorder(root.right, list);
+//     }
+// }
+
+// Method-2
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        List<Integer> list=new ArrayList<>();
-        inorder(root, list);
-        for(int i=0;i<list.size()-1;i++){
-            if(list.get(i)>=list.get(i+1)) return false;
-        }
-        return true;
-
+        return isValidBST(root, null, null);
     }
-    private void inorder(TreeNode root, List<Integer> list){
-        if(root==null) return;
-        inorder(root.left, list);
-        list.add(root.val);
-        inorder(root.right, list);
+    
+    private boolean isValidBST(TreeNode root, Integer low, Integer high) {
+        if(root == null) {
+            return true;
+        }
+
+        if((low != null && root.val <=low) || (high!=null && root.val>=high)) {
+            return false;
+        }
+        return isValidBST(root.left, low, root.val) &&
+        isValidBST(root.right, root.val, high);
     }
 }
