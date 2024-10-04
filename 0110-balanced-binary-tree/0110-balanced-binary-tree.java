97c847a9fb8fd8f998778f95a2cc4e0e75b69640
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2024-10-04 19:32:29 +0530

    Time: 0 ms (100%), Space: 44.3 MB (59.78%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,18 +13,44 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {
+//         if(root==null) return true;
+//         if(Math.abs(height(root.left)-height(root.right))>1) return false;
+//         return isBalanced(root.left) && isBalanced(root.right);
+
+//     }
+
+//     private int height(TreeNode root){
+//         if(root==null) return 0;
+//         int lh=height(root.left);
+//         int rh=height(root.right);
+//         return Math.max(lh,rh)+1;
+//     }
+// }
+
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-        if(Math.abs(height(root.left)-height(root.right))>1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
+        return height(root)!=-1;
+        
 
     }
-
-    private int height(TreeNode root){
-        if(root==null) return 0;
-        int lh=height(root.left);
-        int rh=height(root.right);
-        return Math.max(lh,rh)+1;
+   int height(TreeNode root){
+        TreeNode node= root;
+        if(node==null){
+            return 0;
+        }
+        int lh=height(node.left);
+        if(lh==-1){
+            return -1;
+        }
+        int rh= height(node.right);
+        if(rh==-1){
+            return -1;
+        }
+        if(Math. abs(lh-rh)>1){
+            return -1;
+        }
+        return 1+Math.max(lh,rh);
     }
 }
