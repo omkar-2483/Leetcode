1d1db92dab6d870a71d510cfeb9657df6fd58188
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-01-11 08:17:13 +0530

    Time: 6 ms (40.35%), Space: 53.2 MB (34.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i: nums){
-            if(hm.containsKey(i)){
-                hm.put(i, hm.get(i)+1);
+        // HashMap<Integer, Integer> hm = new HashMap<>();
+        // for(int i: nums){
+        //     if(hm.containsKey(i)){
+        //         hm.put(i, hm.get(i)+1);
+        //     }else{
+        //         hm.put(i, 1);
+        //     }
+        // }
+        // Set<Integer> keys = hm.keySet();
+        // int n= nums.length/2;
+        // for(int i: keys){
+        //     if(hm.get(i) > n) return i;
+        // }
+        // return -1;
+
+        Arrays.sort(nums);
+        int curr = nums[0];
+        int count=1;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]==curr){
+                count++;
+                if(count>nums.length/2) return curr;
             }else{
-                hm.put(i, 1);
+                curr=nums[i];
+                count=1;
             }
         }
-        Set<Integer> keys = hm.keySet();
-        int n= nums.length/2;
-        for(int i: keys){
-            if(hm.get(i) > n) return i;
-        }
-        return -1;
+        return curr;
+
     }
 }
