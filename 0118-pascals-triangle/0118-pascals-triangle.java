ff51aa16a275294808d76ae598de1e3fee14fdd0
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-02-21 09:58:47 +0530

    Time: 1 ms (84%), Space: 41.8 MB (89.31%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,16 +1,22 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+        // in pascal triagle tri[i][j]= ncr(i,j)
         List<List<Integer>> ans = new ArrayList<>();
         for(int i=0; i<numRows; i++){
             ans.add(new ArrayList<>());
             for(int j=0; j<=i; j++){
-                if(j==0 || j==i){
-                    ans.get(i).add(1);
-                }else{
-                    ans.get(i).add(ans.get(i-1).get(j-1)+ans.get(i-1).get(j));
-                }
+                ans.get(i).add((int) ncr(i,j));
             }
         }
         return ans;
+    }
+
+    private long ncr(int n, int r){
+        long res=1;
+        for(int i=0; i<r; i++){
+            res = res * (n-i);
+            res = res / (i+1);
+        }
+        return res;
     }
 }
