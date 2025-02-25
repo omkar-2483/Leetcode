377ff0114b97b02441c52c0cd99f0897d97ec5d3
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-02-25 07:57:50 +0530

    Time: 0 ms (100%), Space: 41.5 MB (98.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,7 +1,40 @@
 class Solution {
     public void sortColors(int[] nums) {
         //heapSort(nums);  // O(N * logN)
-        countSort(nums);  //O(N)
+        //countSort(nums);  //O(N)
+
+        // Dutch National flag algorithm. O(N)
+        DNFsort(nums);
+    }
+
+    private void DNFsort(int[] arr){
+        //rule
+        //arr[0...low-1] = 0
+        //arr[low...mid-1] = 1
+        //arr[mid...high] = unsorted arr
+        //arr[high+1..n-1] = 2
+
+        //we assume whole arr as unsorted
+        int low = 0, mid = 0, high=arr.length-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr,mid,low);
+                low++;
+                mid++;
+            }else if(arr[mid]==1){
+                mid++;
+            }else{  //arr[mid]==2
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+
+    }
+
+    private void swap(int[] arr, int i, int j){
+        int temp=arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     private void countSort(int[] arr){
