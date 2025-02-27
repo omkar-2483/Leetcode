37e05e48c3df8d274f68f13cba2600b02d4cd2a0
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-02-27 16:51:23 +0530

    Time: 0 ms (100%), Space: 40.3 MB (87.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
+    public int uniquePaths2(int m, int n) {
         //paths(i,j) = paths(i+1, j) + paths(i, j+1);
 
         int[][] paths = new int[m][n];
@@ -19,4 +19,27 @@ class Solution {
 
         return paths[0][0];
     }
+
+//space optimization- As we can see to calculate paths[i][j] we need only row i and i+1 i.e. curr and prev row
+    public int uniquePaths(int m, int n) {
+        //paths(i,j) = paths(i+1, j) + paths(i, j+1);
+
+        int[] prev=new int[n];
+
+        for(int i=m-1; i>=0; i--){
+            int[] curr = new int[n];
+            curr[n-1]=1;
+            for(int j=n-2; j>=0; j--){
+                if(i==m-1){ //last row
+                    curr[j]=1;
+                }else{
+                    curr[j]= prev[j]+curr[j+1];
+                }
+            }
+            prev=curr;
+        }
+
+        return prev[0];
+    }
+
 }
