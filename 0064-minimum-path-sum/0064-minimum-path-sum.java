d02339d8377b0c4ef97eb596c43c73aa5c68298d
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-02-27 17:26:07 +0530

    Time: 4 ms (63.1%), Space: 48 MB (22.98%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
+    public int minPathSum2(int[][] grid) {
         //pathSum[i][j] = grid[i][j] + Math.min(pathSum[i+1][j], pathSum[i][j+1])
         int m=grid.length, n=grid[0].length;
         int[][] pathSum = new int[m][n];
@@ -16,5 +16,28 @@ class Solution {
         }
 
         return pathSum[0][0];
+    }
+
+
+    //space optimization -- we use curr row and prev row
+    public int minPathSum(int[][] grid) {
+        //pathSum[i][j] = grid[i][j] + Math.min(pathSum[i+1][j], pathSum[i][j+1])
+        int m=grid.length, n=grid[0].length;
+        int[] prev = new int[n];
+
+        for(int i=m-1; i>=0; i--){
+            int[] curr = new int[n];
+            curr[n-1] = grid[i][n-1] + prev[n-1];
+            for(int j=n-2; j>=0;j--){
+                if(i==m-1){
+                    curr[j] = grid[i][j] + curr[j+1];
+                }else{
+                    curr[j] = grid[i][j] + Math.min(prev[j], curr[j+1]);
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[0];
     }
 }
