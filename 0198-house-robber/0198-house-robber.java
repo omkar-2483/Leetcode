2db9b41ab45922e92ced51f058f839e242785e84
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-02-27 11:38:36 +0530

    Time: 0 ms (100%), Space: 41.3 MB (23.72%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int rob(int[] nums) {
+    public int rob2(int[] nums) {
         //recursion include or exclude home i
         //maxSum(i) =max(nums[i]+maxSum(i-2), maxSum(i-1)) 
         // or maxSum(i)= max(nums[i]+maxSum(i+2), maxSum(i+1))
@@ -19,9 +19,18 @@ class Solution {
 
     
 
-    // public int rob(int[] nums) {
-    //     //space optimization
-        
+    public int rob(int[] nums) {
+        //space optimization
+        if(nums.length==1) return nums[0]; //if only one home
+        int i_2=nums[0];
+        int i_1=Math.max(nums[0], nums[1]);
 
-    // }
+        for(int i=2; i<nums.length; i++){
+            int curr = Math.max(nums[i]+ i_2, i_1);
+            i_2 = i_1;
+            i_1 = curr;
+        }
+        return i_1;
+
+    }
 }
