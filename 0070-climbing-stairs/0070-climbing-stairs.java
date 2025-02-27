66cb32575bc0e6076b47ec094ef04285ba948c91
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-02-27 10:34:25 +0530

    Time: 0 ms (100%), Space: 40.8 MB (17.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,28 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        return helperDP(n, dp);
-    }
+        int prev2=0;
+        int prev1=1;
 
-    private int helperDP(int n, int[] dp){
-        if(n<=2 ) return n;
-
-        if(dp[n]==0){
-            dp[n]=helperDP(n-1, dp)+helperDP(n-2, dp);
+        for(int i=2; i<=n; i++){
+            int curr = prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
         }
-
-        return dp[n];
+        return prev1+prev2;
     }
+
+    // public int climbStairs(int n) {
+    //     int[] dp = new int[n+1];
+    //     return helperDP(n, dp);
+    // }
+
+    // private int helperDP(int n, int[] dp){
+    //     if(n<=2 ) return n;
+
+    //     if(dp[n]==0){
+    //         dp[n]=helperDP(n-1, dp)+helperDP(n-2, dp);
+    //     }
+
+    //     return dp[n];
+    // }
 }
