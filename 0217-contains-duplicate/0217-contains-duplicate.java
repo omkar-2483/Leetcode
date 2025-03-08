d9ee7439bade51dd93049f08ca32dd170cf07250
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-03-08 11:12:14 +0530

    Time: 7 ms (99.32%), Space: 61.3 MB (5.1%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         for(int num: nums){
-            if(set.contains(num)) return true;
-            set.add(num);
+            if(! set.add(num)) return true;
         }
         return false;
     }
