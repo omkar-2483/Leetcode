b1aa7117716ff7a668cab2015c6e876135c7e4a4
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-03-08 11:02:08 +0530

    Time: 2 ms (98.84%), Space: 45.2 MB (17.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum2(int[] nums, int target) {
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
@@ -8,5 +8,18 @@ class Solution {
             }
         }
         return new int[]{};
+    }
+
+    //optimized aaproach
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap <Integer,Integer> hm = new HashMap<>();
+        for(int i=0; i < nums.length; i++){
+            if(hm.containsKey(target - nums[i])){
+                return new int[]{i, hm.get(target-nums[i])};
+            }
+            hm.put(nums[i],i);
+        }
+        return new int[1];
     }
 }
