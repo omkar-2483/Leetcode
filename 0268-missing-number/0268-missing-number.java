b63c5c5ea3f483afef41db9602f4632d9a3b7c81
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-03-10 22:04:07 +0530

    Time: 0 ms (100%), Space: 45.2 MB (83.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,8 +1,26 @@
 class Solution {
-    public int missingNumber(int[] nums) {
+    public int missingNumber2(int[] nums) {
         int n=nums.length;
         int sum=0;
         for(int i: nums) sum+=i;
         return (n* (n+1) / 2) - sum;
     }
+
+    //using xor
+    public int missingNumber(int[] nums){
+        // a ^ a =0
+        // a ^ 0 = a
+        // (xor of 0 to N) ^ (xor of num: nums) = (0^0)^(1^1)...(missing number)...(n^n) = missing number
+        int xor1 = 0;
+        for(int i=0; i<=nums.length; i++){
+            xor1 = xor1 ^ i;
+        }        
+        int xor2=0;
+        for(int num: nums){
+            xor2 = xor2 ^ num;
+        }
+
+        return (xor1 ^ xor2);
+    }
+
 }
