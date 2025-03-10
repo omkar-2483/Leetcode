c2025a1096a8b8c9a0b08d5a1f6b290cecd513dc
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-03-10 18:01:51 +0530

    Time: 0 ms (100%), Space: 45.3 MB (21.17%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     //using prefic product
-    public int maxProduct(int[] nums) {
+    public int maxProduct2(int[] nums) {
         int[] pref = new int[nums.length];
         pref[0] = nums[0];
         for(int i=1; i<nums.length; i++){
@@ -16,5 +16,30 @@ class Solution {
             }
         }
         return maxProd;
+    }
+
+//optimised
+    public int maxProduct(int[] nums) {
+        int prefix = 1;
+        int suffix = 1;
+        int max = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (prefix == 0) {
+                prefix = 1;
+            }
+            prefix = prefix * nums[i];
+            max = Math.max(max,prefix);
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (suffix == 0) {
+                suffix = 1;
+            }
+            suffix = suffix * nums[i];
+            max = Math.max(max,suffix);
+        }
+        return max;
+
     }
 }
