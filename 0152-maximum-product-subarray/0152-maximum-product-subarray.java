aa3436c163711082de7b94c6ed7b8f70c9bd14e1
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-03-10 18:13:33 +0530

    Time: 292 ms (5.54%), Space: 45.1 MB (42.32%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,25 +1,20 @@
 class Solution {
-    //using prefic product
-    public int maxProduct2(int[] nums) {
-        int[] pref = new int[nums.length];
-        pref[0] = nums[0];
-        for(int i=1; i<nums.length; i++){
-            pref[i] =(pref[i-1]!=0) ? pref[i-1]*nums[i]: nums[i];
-        }
-
-        int maxProd=Integer.MIN_VALUE;
+    //brute force
+    public int maxProduct(int[] nums) {
+        int result=nums[0];
         for(int i=0; i<nums.length; i++){
-            for(int j=i; j<nums.length; j++){
-                int curProd = (i==0)? pref[j]: ((pref[i-1]==0) ? pref[j]: pref[j] / pref[i-1]);
-                maxProd = Math.max(maxProd, curProd);
-                if(pref[j]==0) break;
+            int curProd = nums[i];
+            for(int j=i+1; j<nums.length; j++){
+                result = Math.max(result, curProd);
+                curProd*= nums[j];
             }
+            result = Math.max(result, curProd);
         }
-        return maxProd;
+        return result;
     }
 
 //optimised
-    public int maxProduct(int[] nums) {
+    public int maxProduct2(int[] nums) {
         int prefix = 1;
         int suffix = 1;
         int max = nums[0];
