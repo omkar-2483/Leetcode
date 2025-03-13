1cacdc3db79b172b66e8254582b3e3471ba39d8e
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-03-13 11:49:36 +0530

    Time: 45 ms (95.81%), Space: 73.6 MB (59.91%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,12 +1,15 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> (a[0] == b[0])?a[1]-b[1]:a[0]-b[0]);
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]); // Sort by end time
 
-        int count=0;
-        for(int i=1; i<intervals.length; i++){
-            if(intervals[i][0] < intervals[i-1][1]){
-                count++;
-                intervals[i][1]=Math.min(intervals[i][1] ,intervals[i-1][1]);
+        int count = 0;
+        int prevEnd = intervals[0][1]; // Track last non-overlapping interval's end time
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < prevEnd) { // Overlapping case
+                count++; // Remove the current interval
+            } else {
+                prevEnd = intervals[i][1]; // Update prevEnd to the non-overlapping interval
             }
         }
         return count;
