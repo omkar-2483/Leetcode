a3faa78bece4ae7eae7ba3d230f9e30e96ecae67
Omkar Kasture <154991981+omkar-2483@users.noreply.github.com>
2025-03-16 11:58:15 +0530

    Time: 0 ms (100%), Space: 9.2 MB (51.79%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
+    // char findTheDifference(string s, string t) {
+    //     sort(s.begin(), s.end());
+    //     sort(t.begin(), t.end());
+    //     for(int i=0; i<s.length(); i++){
+    //         if(s[i] != t[i]){
+    //             return t[i];
+    //         }
+    //     }
+    //     return t[t.length()-1];
+    // }
+
+    //using xor
     char findTheDifference(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for(int i=0; i<s.length(); i++){
-            if(s[i] != t[i]){
-                return t[i];
-            }
+        // a ^ a =0
+        // a ^ 0 = a
+
+        char ans = s[0];
+        for(int i=1; i<s.length(); i++){
+            ans = ans ^ s[i];
         }
-        return t[t.length()-1];
+        for(char c : t){
+            ans = ans ^ c;
+        }
+        return ans;
     }
 };
